e69e3da398d50cb5226598aeffe2e30f30d01834
Yusuke Izawa <yuizalp@gmail.com>
2017-02-19 01:53:37 +0900

    add simple controller

---
+++ maintenance-cont/src/main/scala/com/github/micchon/app/controller/SimpleController.scala
@@ -0,0 +1,20 @@
+package com.github.micchon.app.controller
+
+import com.github.micchon.app.cont.MaintenanceCont
+import com.github.micchon.app.value.Operation
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
+import play.api.mvc.Controller
+import play.api.mvc.Request
+import play.api.mvc.Results
+import scala.concurrent.Future
+
+class SimpleController extends Controller {
+
+  def index = Action.async { implicit request =>
+    MaintenanceCont.flow(Operation.Transaction)(request) {
+      _ => Future.successful(Results.Ok)
+    }
+  }
+
+}
