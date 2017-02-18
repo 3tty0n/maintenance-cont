88210a476515d0b5d158554f24af8545ff93196f
Yusuke Izawa <yuizalp@gmail.com>
2017-02-18 22:44:49 +0900

    add testRun function

--- maintenance-cont/src/test/scala/com/github/micchon/MaintenanceContSpec.scala
+++ maintenance-cont/src/test/scala/com/github/micchon/MaintenanceContSpec.scala
@@ -1,5 +1,7 @@
 package com.github.micchon
 
+import java.io.File
+
 import com.github.micchon.maintenance.cont._
 import com.github.micchon.maintenance.value.Operation
 import org.scalatest._
@@ -11,6 +13,7 @@ import play.api.test.Helpers._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.sys.process.Process
 
 class MaintenanceContSpec extends FlatSpec {
 
@@ -18,31 +21,49 @@ class MaintenanceContSpec extends FlatSpec {
     implicit val request = FakeRequest()
   }
 
-  "MaintenanceCont" should "メンテナンス状態の場合は 503 ServiceUnavailable" in new SetUp {
-    val cont = MaintenanceCont.flow(Operation.Transaction)(request) {
-      _ => Future.successful(Results.Ok)
+  def testRun[A](name: String)(f: => A): A = {
+    try {
+      val path = "resources/maintenance/operation"
+      Process(s"mkdir -p $path && touch $path/$name.maintenance").run()
+      f
+    } finally {
+      Process(s"rm -rf resources").run()
     }
-    assert(Helpers.status(cont) === Status.SERVICE_UNAVAILABLE)
+  }
+
+  "MaintenanceCont" should "メンテナンス状態の場合は 503 ServiceUnavailable" in new SetUp {
+    testRun(Operation.Transaction.name) {
+      val cont = MaintenanceCont.flow(Operation.Transaction)(request) {
+        _ => Future.successful(Results.Ok)
+      }
+      assert(Helpers.status(cont) === Status.SERVICE_UNAVAILABLE)
+    }
   }
 
   it should "メンテナンス状態でない場合は 200 Ok" in new SetUp {
-    val cont = MaintenanceCont.flow(Operation.Agency)(request) {
-      _ => Future.successful(Results.Ok)
+    testRun(Operation.Transaction.name) {
+      val cont = MaintenanceCont.flow(Operation.Agency)(request) {
+        _ => Future.successful(Results.Ok)
+      }
+      assert(Helpers.status(cont) === Status.OK)
     }
-    assert(Helpers.status(cont) === Status.OK)
   }
 
   "MaintenanceFlowCont" should "メンテナンス状態の場合は 503 ServiceUnavailable" in new SetUp {
-    val action = MaintenanceFlowCont.flow(Operation.Transaction) {
-      _ => Future.successful(Results.Ok)
+    testRun(Operation.Transaction.name) {
+      val action = MaintenanceFlowCont.flow(Operation.Transaction) {
+        _ => Future.successful(Results.Ok)
+      }
+      assert(Helpers.status(action) === Status.SERVICE_UNAVAILABLE)
     }
-    assert(Helpers.status(action) === Status.SERVICE_UNAVAILABLE)
   }
 
   it should "メンテナンス状態でない場合は 200 OK" in new SetUp {
-    val action = MaintenanceFlowCont.flow(Operation.Agency) {
-      _ => Future.successful(Results.Ok)
+    testRun(Operation.Transaction.name) {
+      val action = MaintenanceFlowCont.flow(Operation.Agency) {
+        _ => Future.successful(Results.Ok)
+      }
+      assert(Helpers.status(action) === Status.OK)
     }
-    assert(Helpers.status(action) === Status.OK)
   }
 }
